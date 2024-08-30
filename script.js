e30ac6e715f97dabf4b88780a78d65f6cf325558
ss03006 <siara.saavedra-moreno@stud.th-deg.de>
2024-08-30 18:27:50 +0200

    Css y js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Ejemplo de JavaScript para interactividad simple
+
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav ul li a');
+
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Ajuste de desplazamiento
+                behavior: 'smooth'
+            });
+        });
+    });
+});
